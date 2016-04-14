29674f621f8d0fe27e4fe4e37f3c8e624d7bbbb7
Patrick Bösch <patrick.boesch@viascom.ch>
2016-04-14 16:47:39 +0200

    - junit test changes

--- src/test/Java/ch/viascom/hipchat/api/EmoticonsAPITest.java
+++ src/test/Java/ch/viascom/hipchat/api/EmoticonsAPITest.java
@@ -4,7 +4,6 @@ import ch.viascom.hipchat.api.exception.APIException;
 import ch.viascom.hipchat.api.request.models.GetAllEmoticons;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Test;
 
 /**
  * Created by patrickboesch on 14.04.16.
@@ -15,24 +14,15 @@ public class EmoticonsAPITest {
     private static final String clientToken = "";
     private static final String integrationToken = "";
 
-    @Test
-    public void getEmoticonTest(){
+    //@Test
+    public void getEmoticonTest() throws APIException {
         HipChat hipChat = new HipChat(clientToken);
-        try {
-            hipChat.emoticonsAPI().getEmoticon(19106);
-        } catch (APIException e) {
-            e.printStackTrace();
-            log.debug(e.getErrorResponse());
-        }
+        hipChat.emoticonsAPI().getEmoticon(19106);
     }
 
     //@Test
-    public void getAllEmoticons(){
+    public void getAllEmoticons() throws APIException {
         HipChat hipChat = new HipChat(clientToken);
-        try {
-            hipChat.emoticonsAPI().getAllEmoticons(new GetAllEmoticons());
-        } catch (APIException e) {
-            e.printStackTrace();
-        }
+        hipChat.emoticonsAPI().getAllEmoticons(new GetAllEmoticons());
     }
 }
