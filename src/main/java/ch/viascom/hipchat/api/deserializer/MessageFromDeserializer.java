3a598dd242d6c0a7ca13ddbc958ba453fa03f237
Patrick Bösch <patrick.boesch@viascom.ch>
2016-05-04 14:38:54 +0200

    Use default Deserializer for object-type
    - Use default Deserializer for object-type instead of map each field

--- src/main/java/ch/viascom/hipchat/api/deserializer/MessageFromDeserializer.java
+++ src/main/java/ch/viascom/hipchat/api/deserializer/MessageFromDeserializer.java
@@ -1,10 +1,7 @@
 package ch.viascom.hipchat.api.deserializer;
 
 import ch.viascom.hipchat.api.models.message.MessageFrom;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
+import com.google.gson.*;
 
 import java.lang.reflect.Type;
 
@@ -16,11 +13,8 @@ public class MessageFromDeserializer implements JsonDeserializer<MessageFrom> {
     @Override
     public MessageFrom deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
         MessageFrom messageFrom = new MessageFrom();
-        if(jsonElement.isJsonObject()){
-            messageFrom.setId(jsonElement.getAsJsonObject().get("id").getAsInt());
-            messageFrom.setName(jsonElement.getAsJsonObject().get("name").getAsString());
-            messageFrom.setMention_name(jsonElement.getAsJsonObject().get("mention_name").getAsString());
-            messageFrom.setVersion(jsonElement.getAsJsonObject().get("version").getAsString());
+        if (jsonElement.isJsonObject()) {
+            messageFrom = new Gson().fromJson(jsonElement, MessageFrom.class);
         } else {
             messageFrom.setName(jsonElement.getAsString());
         }
