62fa4e86247d79ed25a89b2b9731079f5e0d69cc
Nikola Stankovic <nikola.stankovic@viascom.ch>
2016-05-06 18:06:29 +0200

    review fixes remove member service
    - README updated
    - review completed

--- src/main/java/ch/viascom/hipchat/api/request/RemoveMemberRequest.java
+++ src/main/java/ch/viascom/hipchat/api/request/RemoveMemberRequest.java
@@ -3,7 +3,6 @@ package ch.viascom.hipchat.api.request;
 import ch.viascom.hipchat.api.request.generic.DeleteRequest;
 import ch.viascom.hipchat.api.request.models.RemoveMember;
 import ch.viascom.hipchat.api.response.NoContentResponse;
-import ch.viascom.hipchat.api.util.JsonUtil;
 import org.apache.http.client.HttpClient;
 
 import java.util.HashMap;
@@ -18,14 +17,6 @@ public class RemoveMemberRequest extends DeleteRequest<NoContentResponse>{
     public RemoveMemberRequest(RemoveMember removeMember, String accessToken, String baseUrl, HttpClient httpClient, ExecutorService executorService){
         super(accessToken, baseUrl, httpClient, executorService);
         this.removeMember = removeMember;
-    }
-
-    @Override
-    protected String getJsonBody() {
-        String json = JsonUtil.getJsonBody(removeMember);
-        // replace second query field
-        json = json.replaceAll("\"userId\":[\"]?(\\w+(\\s+\\w+)*)[\"]?[,]?", "");
-        return json;
     }
 
     @Override
