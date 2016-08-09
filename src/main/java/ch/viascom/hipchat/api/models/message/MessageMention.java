2f968c4e1c7eb54ecd2c6118e1274b55cb460a41
Patrick Bösch <patrick.boesch@viascom.ch>
2016-08-09 07:58:24 +0200

    Improvement for "Mentions returned from history API is empty #15"
    - add attributes to MessageMention

--- src/main/java/ch/viascom/hipchat/api/models/message/MessageMention.java
+++ src/main/java/ch/viascom/hipchat/api/models/message/MessageMention.java
@@ -1,7 +1,16 @@
 package ch.viascom.hipchat.api.models.message;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by patrickboesch on 20.04.16.
  */
+@Data
+@NoArgsConstructor
 public class MessageMention {
+    private int id;
+    private String mention_name;
+    private String name;
+    private String version;
 }
